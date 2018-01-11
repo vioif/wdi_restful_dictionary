d108e0b16a69837874c6aba16ffac875ee177c5e
vioif <ireneaflores3@gmail.com>
2018-01-11 13:58:27 -0500

    generated show page

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,8 @@
 class EntriesController < ApplicationController
+
+  def index
+    # here we'll define some @instance_variables to store data from the database for the views to use
+    render :index
+  end
+
 end
